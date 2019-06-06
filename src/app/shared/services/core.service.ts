0953ab70c373bd32689a3308d08c9c5b08700ace
EOussama <oussama_gtacj@live.fr>
2019-06-06 18:02:38 +0100

    Setup a simple setting architecture

--- src/app/shared/services/core.service.ts
+++ src/app/shared/services/core.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 
+import { Settings } from '../models/settings';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CoreService {
+
+  settings: Settings;
 
   constructor() { }
 }
