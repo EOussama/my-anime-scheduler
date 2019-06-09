2caab042727af662007a9168cbe7c076f1e85454
EOussama <oussama_gtacj@live.fr>
2019-06-09 11:02:00 +0100

    Added styling to the scrolling bar

--- src/app/pages/welcome/animations/routing-animations.ts
+++ src/app/pages/welcome/animations/routing-animations.ts
@@ -1,3 +1,24 @@
-import { trigger } from '@angular/animations';
+import { trigger, transition, style, query, animateChild, group, animate } from '@angular/animations';
 
-export const routeAnimation = trigger('routeAnimations', []);
+export const routeAnimation = trigger('routeAnimations', [
+  transition('ctrlpanel <=> malload', [
+    query(':enter', [
+      style({
+        opacity: '0',
+        transform: 'translateY(-100px)'
+      })
+    ]),
+    query(':leave', [
+      animate('300ms ease-in-out', style({
+        opacity: '0',
+        transform: 'translateY(100px)'
+      }))
+    ]),
+    query(':enter', [
+      animate('300ms ease-in-out', style({
+        opacity: '1',
+        transform: 'translateY(0px)'
+      }))
+    ])
+  ])
+]);
