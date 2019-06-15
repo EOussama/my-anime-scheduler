e23c3792d8101dbdd575dcaf1b08cad416899d8e
EOussama <oussama_gtacj@live.fr>
2019-06-15 11:19:51 +0100

    Added account loading

--- src/app/pages/welcome/components/malload/malload.component.ts
+++ src/app/pages/welcome/components/malload/malload.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 import { MalAccountLoaderService } from 'src/app/pages/welcome/services/mal-account-loader.service';
 import { MalService } from 'src/app/shared/services/mal.service';
+import { promise } from 'protractor';
 
 @Component({
   selector: 'app-malload',
@@ -32,17 +33,31 @@ export class MALloadComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadingForm = new FormGroup({
-      username: new FormControl('', [Validators.required], this.MALAccountValidator)
+      username: new FormControl('', [Validators.required], this.MALAccountValidator.bind(this))
     });
 
     this.confirmationForm = new FormGroup({});
   }
 
   MALAccountValidator(control: FormControl): Promise<any> | Observable<any> {
-    const promise = new Promise<any>((resolve, reject) => {
-      setTimeout(() => {
-        resolve();
-      }, 2000);
+    const username: string = (control.value as string).trim();
+    let validatorTimer: any = null;
+
+    const promise = new Promise<any>(resolve => {
+      clearTimeout(validatorTimer);
+
+      validatorTimer = setTimeout(() => {
+        this.mal.isValid(username).subscribe(
+          res => {
+            console.log('data');
+            resolve(null);
+          },
+          error => {
+            console.log('error');
+            resolve({ "error": true });
+          }
+        );
+      }, 1000);
     });
 
     return promise;
