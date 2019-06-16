45275aaf6623aa479dd600a4542ab781d859803a
EOussama <oussama_gtacj@live.fr>
2019-06-16 15:23:41 +0100

    Added home to welcome redirection

--- src/app/pages/home/guards/auth.guard.ts
+++ src/app/pages/home/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { CoreService } from 'src/app/shared/services/core.service';
@@ -7,9 +7,11 @@ import { CoreService } from 'src/app/shared/services/core.service';
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private core: CoreService) { }
+  constructor(private core: CoreService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    return this.core.account.confirmed;
+    return this.core.account.confirmed
+      ? true
+      : this.router.navigate(['/welcome']);
   }
 }
