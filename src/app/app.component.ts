03dd035a5be50f7c3f4124c4737a836a426ad920
EOussama <oussama_gtacj@live.fr>
2020-08-22 10:57:06 +0100

    ettooo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { create } from 'pkce';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'my-anime-scheduler';
-
-  ngOnInit(): void {
-    const codePair = create();
-    console.log({ codePair });
-  }
 }
