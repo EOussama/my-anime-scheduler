7ea7234b6cee61b126018664a62e66f8cdcd3820
EOussama <oussama_gtacj@live.fr>
2020-08-22 10:45:22 +0100

    Installed a PKCE generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { create } from 'pkce';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-anime-scheduler';
+
+  ngOnInit(): void {
+    const codePair = create();
+    console.log({ codePair });
+  }
 }
